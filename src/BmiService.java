3ea76cf1893c1086bbdbf86a35d85c8f1da9e986
Luba test <flapvj@yandex.ru>
2023-03-28 21:18:20 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,10 @@
+public class BmiService {
+    public int calculate(int weight,double height){
+                double result;
+                result = weight / (height * height);
+                return (int)result;
+
+    }
+
+
+}
